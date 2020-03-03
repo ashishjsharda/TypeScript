0a6a4e12b5f078eb027df48252efe4f6bf9d2ed0
Ashish S <ashishjsharda@gmail.com>
2020-03-03 08:02:11 -0500

    Create vars2.ts

---
+++ vars2.ts
@@ -0,0 +1,9 @@
+/**
+ * Using Variables in Typescript
+ */
+var names:string="Sai";
+var num1:number=20
+var num2:number=10
+var total=num1+num2
+console.log("name = "+names);
+console.log("Total = "+total);
